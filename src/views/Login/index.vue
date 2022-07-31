0062d55fae91e6f63a28e803939130feaa6cb256
chen <11200577+oh-xiao-chen@user.noreply.gitee.com>
2022-07-31 11:22:26 +0800

    我的首页快完成了

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -94,7 +94,7 @@ export default {
         this.$store.commit('SET_TOKEN', token)
 
         // 登录成功之后跳转到profile 我的页面
-        this.$router.push('/profile')
+        this.$router.push('/my')
 
         this.$toast.success('登录成功')
       } catch (e) {
--- src/views/My/index.vue
+++ src/views/My/index.vue
@@ -1,9 +1,33 @@
 <template>
-  <div>我的页面</div>
+  <div>
+    <!-- 头部区域 -->
+    <header>
+      <div class="login" v-if="isLogin">登录后的盒子</div>
+      <div class="logout" v-else>登录前的盒子</div>
+    </header>
+    <!-- 中间区域 -->
+    <main>收藏、历史、消息通知、小智同学</main>
+    <!-- 底部退出区域 -->
+    <footer>
+      <button v-if="isLogin" @click="logout">退出</button>
+    </footer>
+  </div>
 </template>
 
 <script>
-export default {}
+export default {
+  computed: {
+    isLogin() {
+      // !!转成布尔值
+      return !!this.$store.state.tokenObj.token
+    }
+  },
+  methods: {
+    logout() {
+      this.$store.commit('SET_TOKEN', {})
+    }
+  }
+}
 </script>
 
 <style></style>
