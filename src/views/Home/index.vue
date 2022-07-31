dd1d2a004b7bf4811248d3f8747dd5e8e6c2af4f
chen <11200577+oh-xiao-chen@user.noreply.gitee.com>
2022-07-31 14:21:06 +0800

    xx

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,9 +1,38 @@
 <template>
-  <div>首页</div>
+  <div>
+    <!-- 头部 -->
+    <van-nav-bar class="navbar">
+      <template #title>
+        <van-button round class="search-btn" icon="search">搜索</van-button>
+      </template>
+    </van-nav-bar>
+  </div>
 </template>
 
 <script>
 export default {}
 </script>
 
-<style></style>
+<style lang="less" scoped>
+.navbar {
+  background-color: #3296fa;
+  :deep(.van-nav-bar__title) {
+    max-width: unset;
+  }
+
+  :deep(.van-icon) {
+    color: #fff;
+  }
+}
+
+.search-btn {
+  width: 7.4rem;
+  height: 0.85333rem;
+  background-color: #5babfb;
+  color: #fff;
+
+  &.van-button--default {
+    border: 0.02667rem solid #5babfb;
+  }
+}
+</style>
